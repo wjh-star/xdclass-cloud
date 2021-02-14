e4ed54990b89f639b2f8ae789aa9eb5e59c5563e
wjh-star <1075231609@qq.com>
2021-02-14 21:50:06 +0800

    ""

--- xdclass-video-service/src/main/java/net/xdclass/controller/VideoController.java
+++ xdclass-video-service/src/main/java/net/xdclass/controller/VideoController.java
@@ -3,7 +3,6 @@ package net.xdclass.controller;
 import net.xdclass.domain.Video;
 import net.xdclass.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
